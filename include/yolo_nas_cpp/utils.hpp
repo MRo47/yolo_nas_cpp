0177d85cd70e6fa4efafb876c43f8660c5ff777c
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-04-26 15:42:41 +0000

    refactor!: remove unecessary arg in parse_cv_size

--- include/yolo_nas_cpp/utils.hpp
+++ include/yolo_nas_cpp/utils.hpp
@@ -6,12 +6,11 @@ namespace yolo_nas_cpp
 {
 using json = nlohmann::json;
 
-inline cv::Size parse_cv_size(
-  const json & shape_arr, const std::string & param_name = "output_shape")
+inline cv::Size parse_cv_size(const json & shape_arr)
 {
   if (!shape_arr.is_array() || shape_arr.size() != 2) {
     throw std::runtime_error(
-      "'" + param_name + "' must be a JSON array with exactly 2 elements [height, width].");
+      "shape_arr must be a JSON array with exactly 2 elements [height, width].");
   }
   // JSON stores [height, width], cv::Size constructor takes (width, height)
   return cv::Size(shape_arr[1].get<int>(), shape_arr[0].get<int>());
