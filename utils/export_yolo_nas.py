26b020ef7c062e5a277cf0bcfcc5e216310262e2
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-05-18 15:31:39 +0530

    feat: add quantization and openvino export to python script

--- utils/export_yolo_nas.py
+++ utils/export_yolo_nas.py
@@ -1,21 +1,33 @@
-from super_gradients.training import models
-import super_gradients.training.processing as processing
-import numpy as np
-import json
 import argparse
+import json
 import os
 
-def export_onnx(model, model_type, output_path=None, input_shape=(1, 3, 640, 640), opset_version=11):
-    out_file = f"{model_type}.onnx"
+import numpy as np
+import super_gradients.training.processing as processing
+from super_gradients.conversion import ExportQuantizationMode
+from super_gradients.training import models
+
+
+def export_onnx(model, model_type, output_path=None, input_shape=(1, 3, 640, 640), opset_version=11, quantization=None):
+    quantization_mode = ExportQuantizationMode(quantization) if quantization else None
+    model_name = model_type if not quantization else f"{model_type}_{quantization}"
+
+    out_file = f"{model_name}.onnx"
     if output_path:
         os.makedirs(output_path, exist_ok=True)
         out_file = os.path.join(output_path, out_file)
 
     model.eval()
     model.prep_model_for_conversion(input_size=input_shape)
-    model.export(out_file, postprocessing=None, preprocessing=None,
+    model.export(out_file, postprocessing=None, preprocessing=None, quantization_mode=quantization_mode,
                 onnx_export_kwargs={"opset_version":opset_version})
-    
+    return out_file
+
+def export_openvino(onnx_file):
+    import openvino as ov
+    model_path = onnx_file.remove_suffix('.onnx')
+    model = ov.convert_model(onnx_file)
+    ov.serialize(model, f"{model_path}.xml", f"{model_path}.bin")
 
 def get_preprocessing_steps(preprocessing):
     if isinstance(preprocessing, processing.StandardizeImage):
@@ -98,10 +110,12 @@ def export_metadata_get_input_shape(model, model_type, output_path=None) -> tupl
         f.write(json.dumps(metadata))
     return input_shape
 
-def main(model_type, opset_version, output_path=None):
+def main(model_type, opset_version, output_path=None, openvino=False, quantization=None):
     model = models.get(model_type, pretrained_weights="coco")
     input_shape = export_metadata_get_input_shape(model, model_type, output_path=output_path)
-    export_onnx(model, model_type, output_path=output_path,  input_shape=input_shape, opset_version=opset_version)
+    onnx_file = export_onnx(model, model_type, output_path=output_path,  input_shape=input_shape, opset_version=opset_version, quantization=quantization)
+    if openvino:
+        export_openvino(onnx_file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -113,6 +127,9 @@ if __name__ == "__main__":
                         "not the same as onnxruntime version")
     parser.add_argument("--output_path", type=str, default=None, help="Output directory to export files, "
                         "will be exported to CWD if not specified")
+    parser.add_argument("--openvino", action='store_true', help="Export OpenVINO model")
+    parser.add_argument("--quantization", type=str, required=False, choices=["int8", "fp16"], default=None,
+                        help="Quantize model to int8, fp16(requires GPU), or leave unspecified for no quantization")
     args = parser.parse_args()
 
-    main(args.model_type, args.opset_version, args.output_path)
+    main(args.model_type, args.opset_version, output_path=args.output_path, openvino=args.openvino, quantization=args.quantization)
